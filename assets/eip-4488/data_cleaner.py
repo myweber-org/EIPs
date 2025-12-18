b7c5d361c777979f5f20ddb704330286e84fb42b
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-18 22:37:27 +0000

    Add data cleaning utility with outlier removal and normalization functions

--- assets/eip-4488/data_cleaner.py
+++ assets/eip-4488/data_cleaner.py
@@ -89,3 +89,62 @@ def remove_duplicates(sequence):
             seen.add(item)
             result.append(item)
     return result
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+def remove_outliers_iqr(dataframe, column):
+    Q1 = dataframe[column].quantile(0.25)
+    Q3 = dataframe[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return dataframe[(dataframe[column] >= lower_bound) & (dataframe[column] <= upper_bound)]
+
+def remove_outliers_zscore(dataframe, column, threshold=3):
+    z_scores = np.abs(stats.zscore(dataframe[column]))
+    return dataframe[z_scores < threshold]
+
+def normalize_minmax(dataframe, column):
+    min_val = dataframe[column].min()
+    max_val = dataframe[column].max()
+    dataframe[column + '_normalized'] = (dataframe[column] - min_val) / (max_val - min_val)
+    return dataframe
+
+def normalize_zscore(dataframe, column):
+    mean_val = dataframe[column].mean()
+    std_val = dataframe[column].std()
+    dataframe[column + '_standardized'] = (dataframe[column] - mean_val) / std_val
+    return dataframe
+
+def handle_missing_values(dataframe, column, strategy='mean'):
+    if strategy == 'mean':
+        fill_value = dataframe[column].mean()
+    elif strategy == 'median':
+        fill_value = dataframe[column].median()
+    elif strategy == 'mode':
+        fill_value = dataframe[column].mode()[0]
+    else:
+        fill_value = 0
+    
+    dataframe[column] = dataframe[column].fillna(fill_value)
+    return dataframe
+
+def clean_dataset(dataframe, numeric_columns, outlier_method='iqr', normalize_method='minmax', missing_strategy='mean'):
+    cleaned_df = dataframe.copy()
+    
+    for column in numeric_columns:
+        if column in cleaned_df.columns:
+            cleaned_df = handle_missing_values(cleaned_df, column, missing_strategy)
+            
+            if outlier_method == 'iqr':
+                cleaned_df = remove_outliers_iqr(cleaned_df, column)
+            elif outlier_method == 'zscore':
+                cleaned_df = remove_outliers_zscore(cleaned_df, column)
+            
+            if normalize_method == 'minmax':
+                cleaned_df = normalize_minmax(cleaned_df, column)
+            elif normalize_method == 'zscore':
+                cleaned_df = normalize_zscore(cleaned_df, column)
+    
+    return cleaned_df
