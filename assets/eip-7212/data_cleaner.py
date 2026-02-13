7ddbbccd1a99ca9dc4b881309f8e2d59a9e66bcb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-13 15:14:44 +0000

    Add data cleaning utility with outlier removal and normalization functions

--- assets/eip-7212/data_cleaner.py
+++ assets/eip-7212/data_cleaner.py
@@ -281,3 +281,39 @@ def clean_dataset(df, numeric_columns, outlier_method='iqr', normalize_method='m
             cleaned_df = normalize_zscore(cleaned_df, col)
     
     return cleaned_df
+import numpy as np
+import pandas as pd
+
+def remove_outliers_iqr(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+
+def normalize_minmax(df, column):
+    min_val = df[column].min()
+    max_val = df[column].max()
+    df[column + '_normalized'] = (df[column] - min_val) / (max_val - min_val)
+    return df
+
+def standardize_zscore(df, column):
+    mean_val = df[column].mean()
+    std_val = df[column].std()
+    df[column + '_standardized'] = (df[column] - mean_val) / std_val
+    return df
+
+def clean_dataset(df, numeric_columns, outlier_removal=True, normalization='minmax'):
+    cleaned_df = df.copy()
+    
+    for col in numeric_columns:
+        if outlier_removal:
+            cleaned_df = remove_outliers_iqr(cleaned_df, col)
+        
+        if normalization == 'minmax':
+            cleaned_df = normalize_minmax(cleaned_df, col)
+        elif normalization == 'zscore':
+            cleaned_df = standardize_zscore(cleaned_df, col)
+    
+    return cleaned_df
