39b4b6236a0cb289b4b25a32389b19611d6c9a13
InkspillVerse <inkspill.verse@outlook.com>
2026-03-10 23:29:28 +0000

    Add a function to remove outliers using the IQR method

--- assets/eip-3068/data_cleaner.py
+++ assets/eip-3068/data_cleaner.py
@@ -79,3 +79,13 @@ def clean_dataset(df, numeric_columns):
             cleaned_df = zscore_normalize(cleaned_df, col)
     
     return cleaned_df
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
